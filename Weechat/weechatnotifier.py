0dd9e46193d194d50961167fc177e794470407eb
Bernard McKeever <dregin@gmail.com>
2012-06-25 20:41:50 +0100

    Setting up required weechat script import, variables and function.

--- Weechat/weechatnotifier.py
+++ Weechat/weechatnotifier.py
@@ -1,1 +1,59 @@
-# Placeholder for python port of the irssinotifier.pl IRSSI script.
+SCRIPT_NAME = "Weechat Notifier"
+SCRIPT_AUTHOR = "Bernard McKeever"
+SCRIPT_VERSION = "0.1"
+SCRIPT_LICENSE = "GPL3"
+SCRIPT_DESC = " Send notifications to an android phone.\
+                This is a port from IRSSI's IRRSI Notifier\
+                Perl script."
+
+import_ok = True
+
+try:
+    import weechat as w
+except:
+    print("This script must be run under Weechat.\
+         Get weechat now at: http://www.weechat.org")
+
+lastMsg = ""
+lastServer = ""
+lastNick = ""
+lastAddress = ""
+lastTarget = ""
+lastKeyboardActivity = ""
+
+def weechat_init:
+        w.register( SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
+                  SCRIPT_DESC, "", "")
+
+def private:
+    return
+
+def public:
+    return
+
+def print_text:
+    return
+
+def activity_allows_hilight:
+    return
+
+def dangerous_string:
+    return
+
+def hilight:
+    return
+
+def sanitize:
+    return
+
+def encrypt:
+    return
+
+def decrypt:
+    return
+
+def setup_keypress_handler:
+    return
+
+def even_key_pressed:
+    return
